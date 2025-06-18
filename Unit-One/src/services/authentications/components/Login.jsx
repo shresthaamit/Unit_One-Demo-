16d59500e13498a591a9e57a1a8360a1140768fe
Amitstha <shresthaamit273@gmail.com>
2025-06-18 15:21:14 +0545

    Login Completed

--- Unit-One/src/services/authentications/components/Login.jsx
+++ Unit-One/src/services/authentications/components/Login.jsx
@@ -1,10 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { RiAdminFill } from "react-icons/ri";
 
 export default function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const testEmail = "eve.holt@reqres.in";
+    const testPassword = "cityslicka";
+    if (!email || !password) {
+      alert("Email or password is missing");
+      return;
+    }
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(testEmail)) {
+      alert("please enter a valid emailaddress");
+    }
+    fetch("https://reqres.in/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "x-api-key": "reqres-free-v1",
+      },
+      body: JSON.stringify({ email: testEmail, password: testPassword }),
+    })
+      .then((resp) => {
+        if (!resp) {
+          throw new Error("Login Failed");
+        } else {
+          return resp.json();
+        }
+      })
+      .then((data) => {
+        localStorage.setItem("token", data.token);
+
+        alert("Login Successful");
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100 px-4">
-      <form className="bg-white p-8 rounded-xl shadow-md w-full max-w-sm">
+      <form
+        className="bg-white p-8 rounded-xl shadow-md w-full max-w-sm"
+        onSubmit={handleSubmit}
+      >
         <div className="flex justify-center mb-4 text-black-600 text-4xl">
           <RiAdminFill />
         </div>
@@ -16,6 +58,8 @@ export default function LoginForm() {
         <input
           type="email"
           placeholder="admin@example.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           className="w-full px-3 py-2 mb-4 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300"
         />
 
@@ -23,6 +67,8 @@ export default function LoginForm() {
         <input
           type="password"
           placeholder="••••••••"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           className="w-full px-3 py-2 mb-6 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-blue-300"
         />
 
